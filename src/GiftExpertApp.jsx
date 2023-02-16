a3449f51a5ef09efd358356f0241a3c5d10c175e
Jose Saldivar <josedejesus.saldivar.contractor@bbva.com>
2023-02-15 18:13:33 -0600

    Add property onNewCategory in Component GiftExpertApp

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -3,12 +3,14 @@ import { AddCategory } from './components/AddCategory';
 
 export const GiftExpertApp = () => {
   const [categories, setCategories] = useState(['One Punches', 'Dragon Ball']);
-
+  const onAddCategory = (newCategory) => {
+    setCategories([newCategory, ...categories]);
+  };
   return (
     <>
       {/* Titulo*/}
       <h1>GiftExpertApp</h1>
-      <AddCategory setCategories={setCategories} />
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
       <p>KLP5Vx6TKtHQk41odVMthba14HCKRt2n</p>
       <ol>
         {categories.map((category) => {
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const AddCategory = ({ setCategories }) => {
+export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState('');
 
   const onInputChange = ({ target }) => {
@@ -10,7 +10,7 @@ export const AddCategory = ({ setCategories }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (inputValue.trim().length <= 2) return;
-    setCategories((categories) => [inputValue, ...categories]);
+		onNewCategory(inputValue.trim())
     setInputValue('');
   };
 
